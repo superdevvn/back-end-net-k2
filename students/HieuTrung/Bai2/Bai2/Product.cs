8f451e8844b5ea339d4e629050d5b29f4c4bf5d4
HeoBayMau <hieu.trung.030197@gmail.com>
2018-04-02 00:27:53 +0700

    Commit
    Bài 2

---
+++ students/HieuTrung/Bai2/Bai2/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+    public class Product
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+    }
+}
