75c6f4f7e49d1f553631b3b59c3284c25d2b2210
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2018-04-14 19:36:51 +0700

    - Hash MD5

---
+++ source demo/SampleApi/SampleApi/Controllers/UserApiController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+using Models;
+using Models.Common;
+using Newtonsoft.Json.Linq;
+
+namespace SampleApi.Controllers
+{
+    public class UserApiController: ApiController
+    {
+        public string MD5Encrypt(string password)
+        {
+            var md5Hasher = new MD5CryptoServiceProvider();
+            var encoder = new UTF8Encoding();
+            var hashedBytes = md5Hasher.ComputeHash(encoder.GetBytes(password));
+            return hashedBytes.Aggregate(String.Empty, (current, b) => current + b.ToString("x2"));
+        }
+
+        [HttpPost]
+        [Route("api/createUser")]
+        public IHttpActionResult Create(JObject obj)
+        {
+            using (var context = new ApiDbContext())
+            {
+                var user = new User();
+                user.Username = obj["Username"].ToString();
+                user.HashedPassword = MD5Encrypt(obj["Password"].ToString());
+                user.RoleId = new Guid(obj["RoleId"].ToString());
+                user.Id = Guid.NewGuid();
+                user.CreatedDate = DateTime.Now;
+                user.ModifiedDate = DateTime.Now;
+                context.Users.Add(user);
+                context.SaveChanges();
+                return Ok(user);
+            }
+        }
+    }
+}
