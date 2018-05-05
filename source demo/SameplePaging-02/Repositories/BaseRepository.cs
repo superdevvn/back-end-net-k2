c4e54f57bdd642402275fcbf184070f9ee49a551
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2018-05-05 18:24:43 +0700

    Demo Generic Repository

---
+++ source demo/SameplePaging-02/Repositories/BaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Common;
+
+namespace Repositories
+{
+    //Generic Template C#
+    public abstract class BaseRepository<TEntity>
+        where TEntity: class
+    {
+        public virtual TEntity Get(Guid id)
+        {
+            using (var context = new ApiDbContext())
+            {
+                return context.Set<TEntity>().Find(id);
+            }
+        }
+    }
+}
